08accc677b74ed376d48f5dba7c8e986de436b2e
Austin Godber <godber@uberhip.com>
2025-04-01 15:28:31 -0700

    extract job process out into lib.ts

---
+++ app/lib/ts.py
@@ -0,0 +1,84 @@
+def process_source_node(job, logger):
+    op = job['operations'][0]
+
+    if op['_op'] == 'kafka_reader':
+        # souce_node -> kafka_cluster1:topic1
+        source_type = 'KAFKA'
+        source_node = f"{op['connection']}:{op['topic']}"
+    else:
+        logger.warning('MISSING SOURCE')
+
+    return source_node, source_type
+
+def process_destination_node(job, logger):
+    op = job['operations'][-1]
+
+    destination_nodes = []
+    destination_type = None
+
+    if op['_op'] == 'kafka_sender':
+        # destination_node -> kafka_cluster1:topic1
+        destination_nodes.append(f"{op['connection']}:{op['topic']}")
+        destination_type = 'KAFKA'
+    elif op['_op'] == 'elasticsearch_bulk':
+        # destination_node -> es_cluster1:index1
+        destination_nodes.append(f"{op['connection']}:{op['index']}")
+        destination_type = 'ES'
+    elif op['_op'] == 'routed_sender':
+        routed_sender_api = None
+
+        # loop over all APIs to find the one used by the routed sender
+        for api in job['apis']:
+            if api['_name'] == op['api_name']:
+                routed_sender_api = api
+
+        if 'index' in routed_sender_api:
+            destination_type = 'ES'
+        elif 'topic' in routed_sender_api:
+            destination_type = 'KAFKA'
+
+        # suffix is the last part of the destination topic or index
+        # prefix is the beginning part of the destination kafka topic or
+        # elasticsearch index, it comes from the matching api's index or
+        # topic value
+        #
+        for suffix, connection in op['routing'].items():
+            # print(suffix, connection)
+            if destination_type == 'ES':
+                destination_nodes.append(f"{connection}:{api['index']}-{suffix}")
+            elif destination_type == 'KAFKA':
+                destination_nodes.append(f"{connection}:{api['topic']}-{suffix}")
+            else:
+                logger.warning('UNKNOWN!!!!')
+    elif op['_op'] == 'count_by_field':
+        # FIXME: I am not sure how to handle these.
+        logger.debug(f"\t{op}")
+    else:
+        logger.warning('\tMISSING DESTINATION')
+        logger.warning(f"\t{op}")
+    return destination_nodes, destination_type
+
+def process_job(job, logger):
+    """
+    Given a teraslice job, this function will inspect the first and last
+    operators, which are assumed to be the input and outputs of the job
+    and return the following in a tuple
+    * source_node
+      * `<CONNECTOR>:<TOPIC|INDEX>`, e.g. `kafka_cluster1:topic1`
+      * TODO: Kafka only at the moment
+    * source_type
+      * `KAFKA` or `ES`
+    * destination_nodes - an array of one or more `destination_node` strings
+      * [`kafka_cluster1:topic-a-1`, `kafka_cluster1:topic-a-2`]
+    * destination_type
+      * `KAFKA` or `ES`
+    """
+    source_node = None
+    source_type = None
+    destination_nodes = []
+    destination_type = None
+
+    source_node, source_type = process_source_node(job, logger)
+    destination_nodes, destination_type = process_destination_node(job, logger)
+
+    return (source_node, source_type, destination_nodes, destination_type)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import json
 
 import httpx
 
@@ -12,6 +11,7 @@ from fastapi.requests import Request
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from .lib.ts import process_job
 
 # Get settings from Environment with Pydantic BaseSettings
 class Settings(BaseSettings):
@@ -32,91 +32,6 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def get_graph_page(request: Request):
     return templates.TemplateResponse("graph.html", {"request": request})
-
-def process_source_node(job):
-    op = job['operations'][0]
-
-    if op['_op'] == 'kafka_reader':
-        # souce_node -> kafka_cluster1:topic1
-        source_type = 'KAFKA'
-        source_node = f"{op['connection']}:{op['topic']}"
-    else:
-        logger.warning('MISSING SOURCE')
-
-    return source_node, source_type
-
-def process_destination_node(job):
-    op = job['operations'][-1]
-
-    destination_nodes = []
-    destination_type = None
-
-    if op['_op'] == 'kafka_sender':
-        # destination_node -> kafka_cluster1:topic1
-        destination_nodes.append(f"{op['connection']}:{op['topic']}")
-        destination_type = 'KAFKA'
-    elif op['_op'] == 'elasticsearch_bulk':
-        # destination_node -> es_cluster1:index1
-        destination_nodes.append(f"{op['connection']}:{op['index']}")
-        destination_type = 'ES'
-    elif op['_op'] == 'routed_sender':
-        routed_sender_api = None
-
-        # loop over all APIs to find the one used by the routed sender
-        for api in job['apis']:
-            if api['_name'] == op['api_name']:
-                routed_sender_api = api
-
-        if 'index' in routed_sender_api:
-            destination_type = 'ES'
-        elif 'topic' in routed_sender_api:
-            destination_type = 'KAFKA'
-
-        # suffix is the last part of the destination topic or index
-        # prefix is the beginning part of the destination kafka topic or
-        # elasticsearch index, it comes from the matching api's index or
-        # topic value
-        #
-        for suffix, connection in op['routing'].items():
-            # print(suffix, connection)
-            if destination_type == 'ES':
-                destination_nodes.append(f"{connection}:{api['index']}-{suffix}")
-            elif destination_type == 'KAFKA':
-                destination_nodes.append(f"{connection}:{api['topic']}-{suffix}")
-            else:
-                logger.warning('UNKNOWN!!!!')
-    elif op['_op'] == 'count_by_field':
-        # FIXME: I am not sure how to handle these.
-        logger.debug(f"\t{op}")
-    else:
-        logger.warning('\tMISSING DESTINATION')
-        logger.warning(f"\t{op}")
-    return destination_nodes, destination_type
-
-def process_job(job):
-    """
-    Given a teraslice job, this function will inspect the first and last
-    operators, which are assumed to be the input and outputs of the job
-    and return the following in a tuple
-    * source_node
-      * `<CONNECTOR>:<TOPIC|INDEX>`, e.g. `kafka_cluster1:topic1`
-      * TODO: Kafka only at the moment
-    * source_type
-      * `KAFKA` or `ES`
-    * destination_nodes - an array of one or more `destination_node` strings
-      * [`kafka_cluster1:topic-a-1`, `kafka_cluster1:topic-a-2`]
-    * destination_type
-      * `KAFKA` or `ES`
-    """
-    source_node = None
-    source_type = None
-    destination_nodes = []
-    destination_type = None
-
-    source_node, source_type = process_source_node(job)
-    destination_nodes, destination_type = process_destination_node(job)
-
-    return (source_node, source_type, destination_nodes, destination_type)
 
 @app.get("/jobs", response_class=JSONResponse)
 async def get_jobs(size: None | int = 500, active: None | str = 'true', ex: None | str = '_status'):
@@ -140,7 +55,7 @@ async def get_pipeline_graph():
     for job in r:
         logger.debug(f"{job['name']} - {job['ex']['_status']} - {settings.teraslice_url}/jobs/{job['job_id']}",)
 
-        source_node, source_type, destination_nodes, destination_type = process_job(job)
+        source_node, source_type, destination_nodes, destination_type = process_job(job, logger)
 
         nodes.append(
             {
